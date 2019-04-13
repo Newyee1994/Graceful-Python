bcf6b70e091d2ab6d7b88c8cb16717ec523b814c
Newyee1994 <n799230342y@qq.com>
2019-04-13 15:31:08 +0800

    Update Keywords of search_word and city, Update Method of startup_browser

--- Web_Crawler/spider_Lagou.py
+++ Web_Crawler/spider_Lagou.py
@@ -8,7 +8,7 @@
 @Create: 2019-03-15 反反爬、免登录获取「拉勾网」全部职位详情
 @Update: 2019-04-08 静默方式打开浏览器；
                     修复个别 detail_url 页面长时间等待的问题
-@Update: 2019-04-13 配置 SearchWord 和 City 可自定义修改；
+@Update: 2019-04-13 配置 search_word 和 city 可自定义修改；
                     增加 startup_browser 方法而不必两次实例化来重启浏览器
 """
 
